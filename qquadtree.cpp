66f3ec9eadab68662b152814be8cd618a1aecab7
TsubameDono <sebastien.beugnon.pro@gmail.com>
2015-11-27 00:55:39 +0100

    add methods on qquadtree

--- qquadtree.cpp
+++ qquadtree.cpp
@@ -11,6 +11,52 @@ QQuadTree* QQuadTree::parent() {
     return dynamic_cast<QQuadTree*>(QObject::parent());
 }
 
+void QQuadTree::makeChildren()
+{
+
+}
+
+QList<QQuadTree> QQuadTree::getTopNeighbours()
+{
+    return QList<QQuadTree>();
+
+}
+
+QList<QQuadTree> QQuadTree::getBottomNeighbours()
+{
+    return QList<QQuadTree>();
+
+}
+
+QList<QQuadTree> QQuadTree::getLeftNeighbours()
+{
+    return QList<QQuadTree>();
+
+}
+
+QList<QQuadTree> QQuadTree::getRightNeighbours()
+{
+    return QList<QQuadTree>();
+
+}
+
+bool QQuadTree::sameWidth(QQuadTree *neighbour)
+{
+    return false;
+
+}
+
+bool QQuadTree::sameHeight(QQuadTree *neighbour)
+{
+    return false;
+
+}
+
+bool QQuadTree::haveTopNeighbour(QQuadTree *neighbour)
+{
+    return false;
+}
+
 void QQuadTree::setParent(QQuadTree *parent) {
     QObject::setParent(parent);
 }
